f6f6bceca9b93af80d834546419a7aeda50bd333
M0H4MM33D <mohammed.souaidi07@gmail.com>
2018-04-28 14:47:30 +0100

    Create m0h4mm33D.py

---
+++ m0h4mm33D.py
@@ -0,0 +1,491 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# https://www.linkedin.com/in/mohammed-souaidi-576311159/
+ 
+print'''
+# M04MM33D      
+# DONE DONE DONE    
+'''
+ 
+emails = open("email.txt", "r")
+ 
+msn = open("msn.txt", "a")
+
+for email in emails :
+ 
+    if '@msn.' in email :
+ 
+        msn.write(email)
+ 
+       
+ 
+emails.close()
+ 
+msn.close()
+
+
+emails = open("email.txt", "r")
+ 
+bbox = open("bbox.txt", "a")
+
+for email in emails :
+ 
+    if '@bbox.' in email :
+ 
+        bbox.write(email)
+ 
+       
+ 
+emails.close()
+ 
+bbox.close()
+
+
+ 
+emails = open("email.txt", "r")
+ 
+icloud = open("icloud.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@icloud.' in email :
+ 
+        icloud.write(email)
+ 
+       
+ 
+emails.close()
+ 
+icloud.close()
+ 
+  
+
+  
+  
+emails = open("email.txt", "r")
+ 
+apple = open("apple.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@apple.' in email :
+ 
+        apple.write(email)
+ 
+       
+ 
+emails.close()
+ 
+apple.close()
+
+
+
+emails = open("email.txt", "r")
+ 
+mail = open("mail.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@mail.' in email :
+ 
+        mail.write(email)
+ 
+       
+ 
+emails.close()
+ 
+mail.close()
+
+
+emails = open("email.txt", "r")
+ 
+gmx = open("gmx.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@gmx.' in email :
+ 
+        gmx.write(email)
+ 
+       
+ 
+emails.close()
+ 
+gmx.close()
+
+
+
+
+emails = open("email.txt", "r")
+ 
+mac = open("mac.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@mac.' in email :
+ 
+        mac.write(email)
+ 
+       
+ 
+emails.close()
+ 
+mac.close()
+ 
+emails = open("email.txt", "r")
+ 
+me = open("me_icloud.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@me.' in email :
+ 
+        me.write(email)
+ 
+       
+ 
+emails.close()
+ 
+me.close()
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+outlook = open("outlook.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@outlook.' in email :
+ 
+        outlook.write(email)
+ 
+       
+ 
+emails.close()
+ 
+outlook.close()
+ 
+ 
+emails = open("email.txt", "r")
+ 
+live = open("live.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@live.' in email :
+ 
+        live.write(email)
+ 
+       
+ 
+emails.close()
+ 
+live.close()
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+hotmail = open("hotmail.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@hotmail.' in email :
+ 
+        hotmail.write(email)
+ 
+       
+ 
+emails.close()
+ 
+hotmail.close()
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+orange = open("orange.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@orange.' in email :
+ 
+        orange.write(email)
+ 
+       
+ 
+emails.close()
+ 
+orange.close()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+free = open("free.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@free.fr' in email :
+ 
+        free.write(email)
+ 
+       
+ 
+emails.close()
+ 
+free.close()
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+wanadoo = open("wanadoo.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@wanadoo.fr' in email :
+ 
+        wanadoo.write(email)
+ 
+       
+ 
+emails.close()
+ 
+wanadoo.close()
+ 
+ 
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+aol = open("aol.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@aol.' in email :
+ 
+        aol.write(email)
+ 
+       
+ 
+emails.close()
+ 
+aol.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+gmail = open("gmail.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@gmail.' in email :
+ 
+        gmail.write(email)
+ 
+       
+ 
+emails.close()
+ 
+gmail.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+comcast = open("comcast.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@comcast.' in email :
+ 
+        comcast.write(email)
+ 
+       
+ 
+emails.close()
+ 
+comcast.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+charter = open("charter.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@charter.' in email :
+ 
+        charter.write(email)
+ 
+       
+ 
+emails.close()
+ 
+charter.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+yahoo = open("yahoo.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@yahoo.' in email :
+ 
+        yahoo.write(email)
+ 
+       
+ 
+emails.close()
+ 
+yahoo.close()
+ 
+ 
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+SFR = open("SFR.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@sfr.' in email :
+ 
+        SFR.write(email)
+ 
+       
+ 
+emails.close()
+ 
+SFR.close()
+ 
+ 
+emails = open("email.txt", "r")
+ 
+cox = open("cox.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@cox.' in email :
+ 
+        cox.write(email)
+ 
+       
+ 
+emails.close()
+ 
+cox.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+verizon = open("verizon.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@verizon.' in email :
+ 
+        verizon.write(email)
+ 
+       
+ 
+emails.close()
+ 
+verizon.close()
+ 
+ 
+ 
+emails = open("email.txt", "r")
+ 
+att = open("att.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@att.' in email :
+ 
+        att.write(email)
+ 
+       
+ 
+emails.close()
+ 
+att.close()
+
+
+emails = open("email.txt", "r")
+ 
+cogeco = open("cogeco.txt", "a")
+ 
+ 
+ 
+for email in emails :
+ 
+    if '@cogeco.' in email :
+ 
+        cogeco.write(email)
+ 
+       
+ 
+emails.close()
+ 
+cogeco.close()
